da8bb865782f1402e842068b01737494a740670c
abdurshd <kakaabdurashid1995@gmail.com>
2024-09-08 10:18:07 +0900

    fix first message

--- src/prompts/FIRST_QUESTION.js
+++ src/prompts/FIRST_QUESTION.js
@@ -1,12 +1,6 @@
 export const FIRST_MESSAGE = `
 Hi! We're Here for You
-Welcome! 😊
-
-If something is bothering you or making you feel sad or scared, you can tell us. This is a safe place just for you.
-
-What you share with us is private. We’ll only tell someone if you want us to. You’re important, and we care about what you have to say.
-
-Take your time. Whenever you’re ready, we’re here to listen.
+Welcome! If something is bothering you or making you feel sad or scared, you can tell us. This is a safe place just for you.
 `
 
 export default FIRST_MESSAGE;
